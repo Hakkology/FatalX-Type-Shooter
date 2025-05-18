4cd1b5f32d1dc078ef12595869c97318a1c98368
Hakkology <yildizhakan88@gmail.com>
2025-05-18 15:15:10 +0300

    anims

--- Assets/Scripts/Impact/ImpactController.cs
+++ Assets/Scripts/Impact/ImpactController.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class ImpactController : MonoBehaviour
 {
+    private Animator _animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // _animator
     }
 
     // Update is called once per frame
