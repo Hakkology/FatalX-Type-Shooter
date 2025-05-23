a64600ea54d88bb413e59f767aec43aaa9aacaca
Hakkology <yildizhakan88@gmail.com>
2025-05-23 16:57:31 +0300

    socials

--- Assets/Scripts/UI/GUI/Menu/MenuCreditsPanel.cs
+++ Assets/Scripts/UI/GUI/Menu/MenuCreditsPanel.cs
@@ -6,9 +6,13 @@ public class CreditsPanel : BasePanel
     [Header("UI References")]
     [SerializeField] private TextMeshProUGUI headerText;
     [SerializeField] private TextMeshProUGUI developerText;
+    [SerializeField] private TextMeshProUGUI disclaimerText;   
 
     [Header("Content")]
     [SerializeField] private string developerName = "Your Name";
+    [SerializeField, TextArea] 
+    private string disclaimer = 
+@"All assets are randomly added to the game for this portfolio project. Feel free to contact me if you wish your name to be referenced."; 
 
     protected override void Awake()
     {
@@ -16,5 +20,6 @@ public class CreditsPanel : BasePanel
 
         headerText.text     = "FATALIX SOFT\nWEB GAMES";
         developerText.text  = $"Developed by \n{developerName}";
+        disclaimerText.text = disclaimer;   
     }
 }
